f5f42a434ee72ea111a379f6e6a24a5c2f2fb27d
nelsongutierrez <nelson.gutierrez.vega@gmail.com>
2019-03-04 15:07:58 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Post from '../components/Post'
 import Sidebar from '../components/Sidebar'
-import favicon from './nelson.png'
+import favicon from './favicon.png'
 
 class IndexRoute extends React.Component {
   render() {
